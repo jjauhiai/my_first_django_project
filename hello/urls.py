20b2c6f569ae7c18f861b21c78b08aa2099c82af
jjauhiai <jukka.jauhiainen@oamk.fi>
2016-12-05 19:03:27 +0200

    Final version

--- hello/urls.py
+++ hello/urls.py
@@ -19,5 +19,8 @@ urlpatterns=[
     url(r'^bar$', views.bar, name='bar'),
     url(r'^hell$', views.hell,name='hello'),
     url(r'^post_list$', views.post_list,name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^admin/', admin.site.urls)
 ]
